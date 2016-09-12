252d5ed07a97a53babb8f3e21ca1a04543ea0b7f
taingk <taingkn@gmail.com>
2016-09-12 15:57:34 +0200

    fix log

--- server.js
+++ server.js
@@ -167,7 +167,8 @@ io.on("connection", function(socket)
 		io.to(table_id).emit('tableId', table_id, tables, table.game);
 
 		socket.on('get tableId and tableGame', function(id) {
-			console.log(id, tables, get_table(id, tables));
+			console.log(tables);
+
 			io.to(id).emit('give tableId and tableGame', get_table(id, tables));
 		});
 	}
