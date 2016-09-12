d6040f22d7b65ea264716981fa492901b356a94d
taingk <taingkn@gmail.com>
2016-09-12 16:33:07 +0200

    confirm array

--- public/js/sync_sv.js
+++ public/js/sync_sv.js
@@ -204,7 +204,16 @@ function restart() {
 	socket.on('give tableId and tableGame', function(tables) {
 		array = tables;
 	});
+	confirm();
+}
+
+function confirm(){
+	socket.emit('get tableId and tableGame', id);
+	socket.on('give tableId and tableGame', function(tables) {
+		array = tables;
+	});
 	array = get_table(id, array);
 	console.log(array);
-//	socket.emit('re init', get_table(id, array), game);
 }
+
+//	socket.emit('re init', get_table(id, array), game);
