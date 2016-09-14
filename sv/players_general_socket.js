af1796a333195c4b5e9b4d20324d00b872735942
taingk <taingkn@gmail.com>
2016-09-14 11:24:16 +0200

    reset

--- sv/players_general_socket.js
+++ sv/players_general_socket.js
@@ -166,14 +166,15 @@ function	switch_next_player(table, decision)
 	remove_last_actions(table, 3);
 	table.game.highlights_pos = get_next_player(table, table.game);
 	io.to(table.id).emit("highlights", table.game.highlights_pos, "on");
-	send_raise_limits(table, table.game, table.game.highlights_pos, 0, get_seat(table.seats, table.game.highlights_pos).player.bankroll);
+	send_raise_limits(table, table.game, table.game.highlights_pos, 0);
 	adjust_bets_values(table);
+	console.log(get_seat(table.seats, table.game.highlights_pos).player.bankroll);
 	if (get_seat(table.seats, table.game.highlights_pos).player.bankroll) {
 		if (table.game.curbet == "0") {
 			console.log('current bet 0')
 			send_option(table, table.game.highlights_pos, "first choice", "check", 0);
 			send_option(table, table.game.highlights_pos, "second choice", "call", cfg.conf.big_blind);
-			send_raise_limits(table, table.game, table.game.highlights_pos, 1, get_seat(table.seats, table.game.highlights_pos).player.bankroll);
+			send_raise_limits(table, table.game, table.game.highlights_pos, 1);
 		}
 		else {
 			console.log('current bet pas 0, il faut call ou raise')
@@ -200,7 +201,7 @@ function	switch_next_player(table, decision)
 			console.log('3')
 			send_option(table, table.game.highlights_pos, "first choice", "check", 0);
 			send_option(table, table.game.highlights_pos, "second choice", "null", -1);
-			send_raise_limits(table, table.game, table.game.highlights_pos, 1, get_seat(table.seats, table.game.highlights_pos).player.bankroll);
+			send_raise_limits(table, table.game, table.game.highlights_pos, 1);
 		}
 		else {
 			console.log('4');
@@ -253,8 +254,7 @@ function	next_moment(table, game, decision)
 	table.game.highlights_pos = get_first_to_talk(table, game);
 	io.to(table.id).emit("highlights", table.game.highlights_pos, "on");
 	remove_last_actions(table, 3);
-	console.log('log avant bug ' +get_seat(table.seats, table.game.highlights_pos).player.bankroll);
-	send_raise_limits(table, table.game, table.game.highlights_pos, 1, get_seat(table.seats, table.game.highlights_pos).player.bankroll);
+	send_raise_limits(table, table.game, table.game.highlights_pos, 1);
 	if (get_seat(table.seats, table.game.highlights_pos).player.bankroll) {
 		send_option(table, table.game.highlights_pos, "first choice", "check", 0);
 		if (get_seat(table.seats, table.game.highlights_pos).player.bankroll < cfg.conf.big_blind)
