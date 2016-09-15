acc2d81cdd80f6044778ff481f7062a4c1d75b2b
taingk <taingkn@gmail.com>
2016-09-15 15:53:16 +0200

    fix .id

--- sv/players_general_socket.js
+++ sv/players_general_socket.js
@@ -38,19 +38,19 @@ function socket_listens_players(socket, table) {
                         get_seat(table.seats, table.playing_seats[i]).state = "playing";
                     if (table.playing_seats.length > 2)
                         lock = true;
-                    console.log(tableId + ' lock est false, true si + 2 ' + lock);
+                    console.log(tableId.id + ' lock est false, true si + 2 ' + lock);
                     if (lock) {
-                        console.log(tableId + ' lock est true : ' + lock);
+                        console.log(tableId.id + ' lock est true : ' + lock);
                         clearInterval(timer);
                     } else {
-                        console.log(tableId + ' lock est false :' + lock);
-                        io.to(tableId).emit("chrono", 45, "The game will begin ...");
+                        console.log(tableId.id + ' lock est false :' + lock);
+                        io.to(tableId.id).emit("chrono", 45, "The game will begin ...");
                             var timer = setInterval(function() {
                                 lock = true;
                                 new_cashgame(socket, table);
                                 clearInterval(timer);
-                                io.to(tableId).emit("chrono off");
-                                console.log(tableId + ' lock est true : ' + lock);
+                                io.to(tableId.id).emit("chrono off");
+                                console.log(tableId.id + ' lock est true : ' + lock);
                             }, 45000);
                         }
                     }
