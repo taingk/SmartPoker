31263d02f5c41c1a6c84b62e72c12d647bcfa9e2
taingk <taingkn@gmail.com>
2016-09-16 11:01:52 +0200

    get seat

--- public/js/sync_sv.js
+++ public/js/sync_sv.js
@@ -195,6 +195,22 @@ function	sync_sv()
 	});
 }
 
+function	get_seat(seats, idx)
+{
+	if (idx == 1)
+		return seats.seat1;
+	else if (idx == 2)
+		return seats.seat2;
+	else if (idx == 3)
+		return seats.seat3;
+	else if (idx == 4)
+		return seats.seat4;
+	else if (idx == 5)
+		return seats.seat5;
+	else if (idx == 6)
+		return seats.seat6;
+	return 0;
+}
 
 function	get_table(table_id, tables)
 {
