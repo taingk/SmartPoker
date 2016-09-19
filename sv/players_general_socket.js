cdac07c78c419789269f71ac1536d4cd0508ecf2
taingk <taingkn@gmail.com>
2016-09-19 17:40:14 +0200

    test co

--- sv/players_general_socket.js
+++ sv/players_general_socket.js
@@ -36,6 +36,8 @@ function socket_listens_players(socket, table) {
             io.to(table.id).emit("new player", player);
             if (table.playing_seats.indexOf(seat_idx) != -1)
                 return;
+			if (table.playing_seats.length == 1)
+                tryChrono(socket, table);
             if (table.game.moment == "waiting")
                 table.playing_seats.push(seat_idx);
             for (var i = 0; i < table.playing_seats.length; i++)
