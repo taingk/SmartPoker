71813c3f5a37fdf3fee3365cd5019d6966eea0b4
taingk <taingkn@gmail.com>
2016-09-20 16:21:14 +0200

    test check bets

--- sv/players_general_socket.js
+++ sv/players_general_socket.js
@@ -77,7 +77,7 @@ function socket_listens_players(socket, table) {
         treat_decision(table, get_seat(table.seats, seat_nb), decision, bet_amount, get_seat(table.seats, seat_nb).player, seat_nb, rc);
         io.to(table.id).emit("last action", decision, seat_nb, bet_amount);
         console.log(table.game.round_nb + "/" + table.playing_seats.length);
-        if (decision == "FOLD" && table.game.round_nb > table.playing_seats.length) {
+        if (decision == "FOLD" && table.game.round_nb > table.playing_seats.length && check_bets(table, table.seats)) {
             if (table.playing_seats.length < 2)
                 return one_playing_player_left(table);
             next_moment(table, table.game);
