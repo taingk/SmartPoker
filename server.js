752f6d303b9efa3acb8fa58cde9e64b2436b487e
taingk <taingkn@gmail.com>
2016-09-20 14:09:28 +0200

    test redirect url

--- server.js
+++ server.js
@@ -168,9 +168,9 @@ io.on("connection", function(socket) {
         });
 
         socket.on('re init', function(table, game) {
-			var player;
+            var player;
 
-			evalhand(table, game);
+            evalhand(table, game);
             for (idx = 1; idx <= 6; ++idx) {
                 player = get_seat(table.seats, idx).player;
                 console.log(player);
@@ -180,19 +180,20 @@ io.on("connection", function(socket) {
         });
     }
     if (device_client) {
-		if (!get_private_id(table.private_ids, seat_nb)) {
-        	console.log(table.private_ids);
-        	private_channel = table_id + seat_nb; //shortId.generate() + seat_nb;
+        if (!get_private_id(table.private_ids, seat_nb)) {
+            console.log(table.private_ids);
+            private_channel = table_id + seat_nb; //shortId.generate() + seat_nb;
             table.private_ids.push(private_channel);
-	        socket.on("get private channel", function() {
-	            socket.emit("private channel", private_channel, seat_nb);
-	        });
-	        socket.join(private_channel);
-	        console.log("Joining private channel " + private_channel);
-	        hide_qr(table, seat_nb);
-		} else {
-			console.log('Seat busy');
-			window.location = "http://poker.smartgames.tv/page_not_found.html";
+            socket.on("get private channel", function() {
+                socket.emit("private channel", private_channel, seat_nb);
+            });
+            socket.join(private_channel);
+            console.log("Joining private channel " + private_channel);
+            hide_qr(table, seat_nb);
+        }
+		else {
+			console.log("Seat busy");
+			io.to(table.id).emit("redirect url");
 		}
     }
     socket.on("disconnect", function() {
