2212630c9c5bb6c5fdaf488dde207b49036aa454
taingk <taingkn@gmail.com>
2016-09-20 15:15:38 +0200

    fix dc

--- sv/table_general_socket.js
+++ sv/table_general_socket.js
@@ -71,7 +71,7 @@ function socket_listens_global_settings(socket, table, private_channel) {
                 console.log('in waiting dc');
                 remove_from_seat_array(table, socket_nickname);
                 remove_from_playing_seats(table.playing_seats, player_seat_idx);
-				console.log("table playing seats " + table.playing_seats);
+                console.log("table playing seats " + table.playing_seats);
                 io.to(table.id).emit("bet", player_seat_idx, 0);
                 io.to(table.id).emit("kick player", player_seat_idx);
                 if (table.players_nb > 0)
@@ -80,23 +80,26 @@ function socket_listens_global_settings(socket, table, private_channel) {
                 console.log('in game dc');
                 remove_from_seat_array(table, socket_nickname);
                 remove_from_playing_seats(table.playing_seats, player_seat_idx);
-				var seatPlayer = table.seats.seat + player_seat_idx;
-				console.log(seatPlayer);
-				console.log("table playing seats " + table.playing_seats);
-				console.log(table);
+                var seatPlayer = table.seats.seat + player_seat_idx;
+                console.log(seatPlayer);
+                console.log("table playing seats " + table.playing_seats);
+                console.log(table);
                 if (player_seat_idx == table.game.highlights_pos) {
                     io.to(table.id).emit("highlights", table.game.highlights_pos, "off");
                     table.game.highlights_pos = 0;
-					if (table.game.round_nb > table.playing_seats.length) {
-						console.log("1");
-			            next_moment(table, table.game);
-			        } else if (table.game.round_nb == table.playing_seats.length) {
-						console.log("2");
-			            switch_next_player(table)
-			        } else {
-						console.log("3");
-			            switch_next_player(table);
-			        }
+                    if (table.playing_seats.length == 1) {
+                        console.log('one player left');
+                    }
+                    else if (table.game.round_nb > table.playing_seats.length) {
+                        console.log("1");
+                        next_moment(table, table.game);
+                    } else if (table.game.round_nb == table.playing_seats.length) {
+                        console.log("2");
+                        switch_next_player(table)
+                    } else {
+                        console.log("3");
+                        switch_next_player(table);
+                    }
                 }
                 io.to(table.id).emit("bet", player_seat_idx, 0);
                 io.to(table.id).emit("kick player", player_seat_idx);
