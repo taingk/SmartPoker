87973f0b894f361820dbdbc3792d583204868b4b
taingk <taingkn@gmail.com>
2016-09-20 15:09:56 +0200

    fix seat

--- sv/table_general_socket.js
+++ sv/table_general_socket.js
@@ -81,7 +81,7 @@ function socket_listens_global_settings(socket, table, private_channel) {
                 remove_from_seat_array(table, socket_nickname);
                 remove_from_playing_seats(table.playing_seats, player_seat_idx);
 				var seatPlayer = table.seats.seat + player_seat_idx;
-				seatPlayer[player] = -1;
+				console.log(seatPlayer);
 				console.log("table playing seats " + table.playing_seats);
 				console.log(table);
                 if (player_seat_idx == table.game.highlights_pos) {
