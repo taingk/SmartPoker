26110d9cf033d81e289b4461694e43aacd23e82c
taingk <taingkn@gmail.com>
2016-09-23 16:54:54 +0200

    tset

--- sv/table_general_socket.js
+++ sv/table_general_socket.js
@@ -69,7 +69,6 @@ function socket_listens_global_settings(socket, table, nb_seat) {
             socket_nickname = nickname;
     });
     socket.on("disconnect", function() {
-        console.log('JME DECO');
         if (socket_nickname) {
             player_seat_idx = get_player_seat_by_nickname(get_table(table.id, tables).seats, socket_nickname);
             private_channelx = get_table(table.id, tables).id + player_seat_idx;
@@ -114,7 +113,6 @@ function socket_listens_global_settings(socket, table, nb_seat) {
                     return one_playing_player_left(table);
                 }
             }
-            console.log('SALUT');
             socket.leave(private_channelx);
             socket.disconnect();
         } else {
