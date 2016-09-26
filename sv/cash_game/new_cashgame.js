f7c7583512efa13169539306862e68d43f2d34fe
taingk <taingkn@gmail.com>
2016-09-26 13:38:14 +0200

    loading reset

--- sv/cash_game/new_cashgame.js
+++ sv/cash_game/new_cashgame.js
@@ -254,6 +254,7 @@ function stop_high_rollers(table) {
 
 function new_cashgame(socket, table) {
     init_obj(table);
+	io.to(table.id).emit("highlights", table.game.highlights_pos, "off");
     table.game.moment = "preflop";
     table.game.round_nb = socket === 42 ? 0 : 1;
     table.game.pot_amount = 0;
