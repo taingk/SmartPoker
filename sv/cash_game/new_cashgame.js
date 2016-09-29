907bca6862c78ffcbbf56a661891c8143fafcb26
taingk <taingkn@gmail.com>
2016-09-29 14:43:29 +0200

    fold issue

--- sv/cash_game/new_cashgame.js
+++ sv/cash_game/new_cashgame.js
@@ -190,7 +190,7 @@ function ask_first_player(table, game) {
     //start_timer(table, get_seat(table.seats, game.highlights_pos).player.nickname);
     send_option(table, game.highlights_pos, "first choice", "call", /*game.highlights_pos === game.sb_pos ? game.curbet / 2 :*/ game.curbet);
     send_option(table, game.highlights_pos, "second choice", "null", -1 /*game.highlights_pos === game.sb_pos ? game.curbet * 2 / 2 : game.curbet */ );
-    send_option(table, game.highlights_pos, "third choice", "fold");
+    send_option(table, game.highlights_pos, "third choice", "fold", game.highlights_pos, 0);
     /* SEE PLAYER GENERAL SOCKETS FOR THE FOLLOWING */
 }
 
