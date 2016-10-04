222028ab24ba021c7b1ea8279216a6e727100b73
taingk <taingkn@gmail.com>
2016-10-04 13:42:46 +0200

    test fold

--- sv/players_general_socket.js
+++ sv/players_general_socket.js
@@ -56,18 +56,17 @@ function socket_listens_players(socket, table) {
             io.to(table.id).emit("send river", table.game.board[4]);
     });
     socket.on("player decision", function(decision, channel_id, bet_amount, rc) {
-		if (decision == "FOLD" || decision == "PASS")
-			console.log('Je fold ou je pass ' + decision, channel_id);
         if (!decision || !channel_id)
             return;
         var seat_nb = +channel_id[channel_id.length - 1];
         var player = get_seat(table.seats, seat_nb).player;
 
-        if (table.playing_seats.length == 2)
-            table.game.highlights_pos = seat_nb;
-		console.log('Salut je voulais fold '+seat_nb, table.game.highlights_pos);
-        if (seat_nb != table.game.highlights_pos)
-            return;
+        if (seat_nb != table.game.highlights_pos) {
+			if (table.playing_seats.length == 2)
+	            table.game.highlights_pos = seat_nb;
+			else
+				seat_nb = table.game.highlights_pos;
+		}
         if (bet_amount && bet_amount[bet_amount.length - 1] == "$") {
             bet_amount = bet_amount.slice(0, bet_amount.length - 1);
             bet_amount = (Math.round(+bet_amount * 100)) / 100;
