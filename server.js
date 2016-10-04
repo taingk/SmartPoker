4065c401984a0aa05a54f108a6f0bec871d6f3aa
taingk <taingkn@gmail.com>
2016-10-04 15:26:49 +0200

    test pingpong

--- server.js
+++ server.js
@@ -177,15 +177,12 @@ io.on("connection", function(socket) {
             socket.disconnect();
         }
     }
-    socket.on('pong', function(data) {console.log('Pong');});
-    setTimeout(sendHeartbeat, 10000);
-
-    function sendHeartbeat() {
-        setTimeout(sendHeartbeat, 10000);
-        io.sockets.emit('ping', {
-            beat: 1
-        });
-    }
+    setInterval(function(){
+		io.to(table.id).emit('ping', {beat: 1});
+	}, 10000);
+	socket.on('pong', function(data) {
+		console.log(data);
+	});
     send_bets(table); // Currents bets on the table.
     socket_listens_players(socket, table);
     socket_listens_global_settings(socket, table, seat_nb); // Event handler for major events.
