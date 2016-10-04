31fc585db576d7f2a01728a328bf71715846ee92
taingk <taingkn@gmail.com>
2016-10-04 14:18:25 +0200

    test

--- sv/cash_game/timer.js
+++ sv/cash_game/timer.js
@@ -1,6 +1,4 @@
 function stop_timer(table) {
     console.log("AFK fold");
-    console.log('first private id ' + get_private_id(table.private_ids, table.game.highlights_pos));
-    console.log('second private id ' + get_private_id(get_table(table.id, tables).private_ids, get_table(table.id, tables).game.highlights_pos));
-    io.to(table.id).emit("i fold", "FOLD", get_private_id(table.private_ids, table.game.highlights_pos), 0);
+    io.to(table.id).emit("i fold", "FOLD", get_private_id(get_table(table.id, tables).private_ids, get_table(table.id, tables).game.highlights_pos), 0);
 }
