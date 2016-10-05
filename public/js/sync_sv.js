50abaea0ebebb0b29278510f81b2890eb5d6b573
taingk <taingkn@gmail.com>
2016-10-05 11:37:58 +0200

    try

--- public/js/sync_sv.js
+++ public/js/sync_sv.js
@@ -231,6 +231,7 @@ function sync_sv() {
         $("#winner, #card1_1, #card2_1, #card1_2, #card2_2, #card1_3, #card2_3, #card1_4, #card2_4, #card1_5, #card2_5, #card1_6, #card2_6").css("visibility", "hidden");
     });
 	socket.on("start game", function(socket, table){
+		console.log('start game');
 		if (lock)
 			return;
 		else
