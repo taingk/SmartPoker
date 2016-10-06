6e9920ed0f59e802b5798acda9ceefb36c2eae9a
taingk <taingkn@gmail.com>
2016-10-06 14:25:23 +0200

    evalwin

--- sv/cash_game/show_down.js
+++ sv/cash_game/show_down.js
@@ -1,60 +1,66 @@
-function evalwin(table, game) {
-    var winners = new Array();
-    var idx = 1;
-    var best = 0;
-    var player;
+function	evalwin(table, game)
+{
+	var		winners = new Array();
+	var		idx = 1;
+	var		best = 0;
+	var		player;
 
-    while (idx <= 6) {
-        if (get_seat(table.seats, idx).state != "busy")
-            player = get_seat(table.seats, idx).player;
-        if (player.rank_value) {
-            if (!best || player.rank_value > best.rank_value)
-                best = player;
-            else if (player.rank_value === best.rank_value)
-                winners.push(player);
-        } else {
-            return 0;
-        }
-        ++idx;
-    }
-    if (!winners.length)
-        winners.push(best);
-	console.log(winners);
-    return winners;
+	while (idx <= 6)
+	{
+		if (get_seat(table.seats, idx).state != "busy")
+			player = get_seat(table.seats, idx).player;
+		if (player.rank_value)
+		{
+			console.log('Je rentre ici');
+			if (!best || player.rank_value > best.rank_value)
+				best = player;
+			else if (player.rank_value === best.rank_value)
+				winners.push(player);
+		}
+		++idx;
+	}
+	if (!winners.length)
+		winners.push(best);
+		console.log('winners array '+ winners);
+	return winners;
 }
 
-function show_down(table, game) {
-    var winners; // Indeed, with a 's' at the end in case of split.
-    var player;
+function	show_down(table, game)
+{
+	var		winners; // Indeed, with a 's' at the end in case of split.
+	var		player;
 
-    table = get_table(table.id, tables);
-    table.game.moment = "waiting end game";
-    if (!(winners = evalwin(table, game))) {
-        console.log("Can't retrieve winner(s)! Game error!");
-		winners = 42;
+	table = get_table(table.id, tables);
+	table.game.moment = "waiting end game";
+	if (!(winners = evalwin(table, game)))
+		console.log("Can't retrieve winner(s)! Game error!");
+	console.log("And the winner is..." + winners[0].nickname);
+
+	for (var i = 1; i < 7; i++) {
+		io.to(get_private_id(table.private_ids, i)).emit("cancel buttons", "hidden");
+	}
+
+	if (winners.length == 1)
+	{
+		winners[0].bankroll += game.pot_amount;
+		io.to(table.id).emit("bankroll modification", winners[0].seat_nb, winners[0]);
 	}
 	else
-    	console.log("And the winner is..." + winners[0].nickname);
-
-    for (var i = 1; i < 7; i++) {
-        io.to(get_private_id(table.private_ids, i)).emit("cancel buttons", "hidden");
-    }
-
-    if (winners.length == 1) {
-        winners[0].bankroll += game.pot_amount;
-        io.to(table.id).emit("bankroll modification", winners[0].seat_nb, winners[0]);
-    } else {
-        console.log("Split!");
-        for (var idx = 0; idx < winners.length; ++idx) {
-            winners[idx].bankroll += game.pot_amount / winners.length;
-            io.to(table.id).emit("bankroll modification", winners[idx].seat_nb, winners[idx]);
-        }
-    }
-    for (idx = 1; idx <= 6; ++idx) {
-        if (table.playing_seats.indexOf(idx) != -1) {
-            player = get_seat(table.seats, idx).player;
-            io.to(table.id).emit("show down", player.card1, player.card2, idx);
-        }
-    }
-    return end_game(table, game, winners, player);
+	{
+		console.log("Split!");
+		for (var idx = 0; idx < winners.length; ++idx)
+		{
+			winners[idx].bankroll += game.pot_amount / winners.length;
+			io.to(table.id).emit("bankroll modification", winners[idx].seat_nb, winners[idx]);
+		}
+	}
+	for (idx = 1; idx <= 6; ++idx)
+	{
+		if (table.playing_seats.indexOf(idx) != -1)
+		{
+			player = get_seat(table.seats, idx).player;
+			io.to(table.id).emit("show down", player.card1, player.card2, idx);
+		}
+	}
+	return end_game(table, game, winners, player);
 }
